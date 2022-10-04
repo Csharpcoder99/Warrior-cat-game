1f2641fbd82a0e0940f9249300f46c9b98c20fdd
C++ Programmer <yosefysrayl01@gmail.com>
2022-10-04 12:30:31 +0900

    added some main gameplay

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,76 @@
 #include <iostream>
+#include <unistd.h>
+#include <fstream>
 
+void Option(){
+    std::string answer;
+    std::cout << "Options: ";
+    std::cout << "1.find new warriors \n";
+    std::cout << "2.train \n";
+    std::cout << "3.Inventory \n";
+    std::cout << "4.Quit \n";
+
+    std::cout << "Type>> ";
+    std::cin.ignore();
+    std::getline(std::cin, answer, '*');
+}
 void Menu()
 {
 
     for (int i = 0; i < 1; i++)
     {
 
-        for (int j = 0; j < 1; j++){
-
-            
+        for (int j = 0; j < 1; j++)
+        {
+            std::cout << "###############\n";
+            std::cout << "#1.Start Game \n";
+            std::cout << "#2.Quit Game  \n";
+            std::cout << "###############\n\n";
+            std::cout << "Type: ";
         }
     }
 }
 
 int main()
 {
-    Menu();
 
-    std::string userCharacters[7] = {
+    std::string region[5] = {
+        "Nothing",
+        "Desert",
+        "Jungle",
+        "Arctic",
+        "Sea"
+    };
+
+    int option;
+    int movement;
+    int cats = 2;
+    int trainingItems;
+    int picktrainingItem;
+
+
+    srand((unsigned)time(0));
+    int spawnWarrior = 1 + (rand() % 7);
+
+    srand((unsigned)time(0));
+    int random_number = 1 + (rand() % 4);
+
+    std::string uncatchCats[8] = {
+        // this array goes from top to bottom the top cats are the rare ones
+        "Nothing",
+        "Tigerstar",
+        "Sandstorm",
+        "Brambleclaw",
+        "Bluestar",
+        "Leafpool",
+        "Feathertail",
+        "Spottedleaf",
+    };
+
+    std::string userCharacters[8] = {
+        "Nothing",
         "Pinestar",
         "Yellowfang",
-
     };
 
     class Pinestar
@@ -80,11 +130,115 @@ int main()
 
     Yellofang Yellofang;
     Yellofang.damage = 55;
-    Yellofang.health = 23; 
+    Yellofang.health = 23;
     Yellofang.specialmoveName = "Healer";
 
     Yellofang.specialmovehealingPoint = 35;
     Yellofang.name = userCharacters[2];
     Yellofang.level = 1;
 
+    Menu();
+    std::cin >> option;
+
+    if (option == 1)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            system("clear");
+            std::cout << "You have " << Pinestar.name << ", and " << Yellofang.name << std::endl;
+
+            sleep(2);
+            system("clear");
+
+            switch (random_number)
+            {
+
+                case 1:
+                    std::cout << "You are in the region of " << region[1] << std::endl;
+                    break;
+
+                case 2:
+                    std::cout << "You are in the region of " << region[2] << std::endl;
+                    break;
+                case 3:
+                    std::cout << "You are in the region of " << region[3] << std::endl;
+                    break;
+                case 4:
+                    std::cout << "You are in the region of " << region[4] << std::endl;
+                    break;
+                default:
+                    std::cout << "You are in the region of " << region[1] << std::endl;
+                    break;
+
+                
+            }
+
+            sleep(2);
+            system("clear");
+            Option();
+
+            std::cin >> movement;
+
+            switch(movement){
+                case 1:
+                    std::cout << "Looking for a warrior to catch \n";
+                    sleep(1);
+
+                    switch(spawnWarrior){
+                        case 1:
+                            std::cout << "You caught " << uncatchCats[8] << std::endl;
+                            break;
+                        case 2:
+                            std::cout << "You caught " << uncatchCats[7] << std::endl;
+                            break;
+                        case 3:
+                            std::cout << "You caught " << uncatchCats[6] << std::endl;
+                            break;
+                        case 4:
+                            std::cout << "You caught " << uncatchCats[5] << std::endl;
+                            break;
+                        case 5:
+                            std::cout << "You caught " << uncatchCats[4] << std::endl;
+                            break;
+                        case 6:
+                            std::cout << "You caught " << uncatchCats[3] << std::endl;
+                            break;
+                        case 7:
+                            std::cout << "You caught " << uncatchCats[2] << std::endl;                            
+                            break;
+                        case 8:
+                            std::cout << "You caught " << uncatchCats[1] << std::endl;
+                            break;
+
+                    }
+                case 2:
+
+                    if (trainingItems > 1){
+                        std::cout << "Please select which training item you want to use \n";
+                        std::cout << std::endl;
+
+                        std::cout << "########Training Items######### \n";
+                        std::cout << "# 1.Xp Gainer(This gains more xp for the warrior without battling)\n";
+                        std::cout << "# 2.Health replenish \n";
+                        std::cout << "#########Traing Items########## \n";
+
+                        std::cout << "Type: ";
+                        std::cin >> picktrainingItem;
+
+                        if (picktrainingItem == 1){
+                            std::cout << "Which warrior: " << Pinestar.name << " " << Yellofang.name << std::endl;
+                        }
+                    }else if (trainingItems < 1){
+                        std::cout << "You do not have any training items \n";
+                        continue;
+                    }
+            }
+
+
+        }
+    }
+    else if (option == 2)
+    {
+        exit(EXIT_SUCCESS);
+    }
 };
